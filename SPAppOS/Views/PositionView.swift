f8b038f20431a8ab131b6ba3f41d29ba468d4e02
Preston Smith <prestonsmith657@gmail.com>
2020-04-29 10:55:58 -0400

    removed comment

--- SPAppOS/Views/PositionView.swift
+++ SPAppOS/Views/PositionView.swift
@@ -131,7 +131,6 @@ struct PositionView: View {
                         .font(.system(size: UIScreen.main.bounds.width * 0.0375))
                         .padding(5)
                 }.padding(15)
-                // PROBLEM HERE -- This only advances days so far, but going back would be pretty simple if we can figure this out
             }.animation(.spring())
                 .offset(x: self.dragOffset.width)
                 .gesture(DragGesture().onChanged{value in
